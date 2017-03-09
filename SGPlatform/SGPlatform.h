1570355eaef29d1afb1504f2d9ec62f29ac019f5
Single <musicman_leehom@126.com>
2017-03-09 11:42:47 +0800

    Initial Commit

---
+++ SGPlatform/SGPlatform.h
@@ -0,0 +1,19 @@
+//
+//  SGPlatform.h
+//  SGPlatform
+//
+//  Created by Single on 2017/3/9.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SGPlatform.
+FOUNDATION_EXPORT double SGPlatformVersionNumber;
+
+//! Project version string for SGPlatform.
+FOUNDATION_EXPORT const unsigned char SGPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGPlatform/PublicHeader.h>
+
+
