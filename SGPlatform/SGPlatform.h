9ec4e21735141092470453735b7b0a59dfd5da8e
Single <musicman_leehom@126.com>
2017-03-09 12:11:04 +0800

    init

--- SGPlatform/SGPlatform.h
+++ SGPlatform/SGPlatform.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 single. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SGPlatform.
 FOUNDATION_EXPORT double SGPlatformVersionNumber;
